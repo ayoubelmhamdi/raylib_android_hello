1594bedc802670fdeb771d65c10d9b9341f4c3b5
Ayoub EL Mhamdi <ayoub.elmhamdi@gmail.com>
2024-04-19 12:56:01 +0100

    up

--- src/raylib_game.c
+++ src/raylib_game.c
@@ -1,15 +1,36 @@
 #include "raylib.h"
 
-int main(void)
-{
-    InitWindow(800, 600, "Ayoub Android");
+#define BORDER_SIZE 2
 
-    while (!WindowShouldClose()){
+#define TSODING CLITERAL(Color){0x18, 0x18, 0x18, 0xFF}
+
+void DrawGameFrame(int w, int h) {
+    ClearBackground(BLUE);
+    DrawRectangle(BORDER_SIZE, BORDER_SIZE, w - 2 * BORDER_SIZE, h - 2 * BORDER_SIZE, TSODING);
+    DrawCircle(w / 2, h / 2, w / 8, RED);
+}
+
+int main(void) {
+    SetConfigFlags(FLAG_WINDOW_RESIZABLE);
+    InitWindow(0, 0, "Ayoub");
+
+    int w = GetScreenWidth();
+    int h = GetScreenHeight();
+
+    SetWindowSize(w, h);
+    SetWindowPosition(w / 2, h / 2);
+
+    SetTargetFPS(60);
+
+    while (!WindowShouldClose()) {
+        w = GetScreenWidth();
+        h = GetScreenHeight();
         BeginDrawing();
-            ClearBackground(RED);
+            DrawGameFrame(w, h);
         EndDrawing();
     }
 
     CloseWindow();
     return 0;
 }
+
