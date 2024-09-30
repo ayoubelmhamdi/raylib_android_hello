11480256a55d556bfaa6fc301edd297dbbc8cdfa
Ayoub EL Mhamdi <ayoub.elmhamdi@gmail.com>
2024-09-30 02:20:25 +0100

    (build_linux.sh) more rebostic build

--- build_linux.sh
+++ build_linux.sh
@@ -1,9 +1,21 @@
 #!/bin/sh
-CFLAGS="-Wall -Wextra -pedantic -std=c99 -ggdb -O0"
+
+DEBUG_CFLAGS="-ggdb -O0"
+RELEASE_CFLAGS="-O3"
+
+CFLAGS="-Wall -Wextra -pedantic -std=c99"
 INCLUDES="-I. -Iinclude -I../include"
 LIBS="-lm -lglfw -ldl -lpthread ./lib/linux/libraylib.a"
 
 
 mkdir -p build/
-gcc $CFLAGS $INCLUDES ./src/raylib_game.c -o ./game $LIBS
+if [ $RELEASE ];then
+    echo "Release mode"
+    set -x
+    gcc $CFLAGS $RELEASE_CFLAGS $INCLUDES ./src/raylib_game.c -o ./game $LIBS
+else
+    echo "Debug mode"
+    set -x
+    gcc $CFLAGS $DEBUG_CFLAGS   $INCLUDES ./src/raylib_game.c -o ./game $LIBS -lX11
+fi
 
